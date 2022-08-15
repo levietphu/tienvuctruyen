bacd970f18708699d57d28fff7e971166ee63850
unknown <levietphu171993@gmail.com>
2022-08-15 09:13:44 +0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './app.scss';
+
+function App() {
+
+  return (
+    <div>
+      {/* <div className='nav__left'>
+          chào các bạn
+      </div>
+      <div className='nav__right'>
+        hay quá
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
