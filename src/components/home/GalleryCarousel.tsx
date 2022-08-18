819abcdb1570bb9c84f56218750ec205a6f4e42c
unknown <levietphu171993@gmail.com>
2022-08-18 16:08:06 +0700

    add

--- src/components/home/GalleryCarousel.tsx
+++ src/components/home/GalleryCarousel.tsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import home1 from "../../assets/1.jpg";
 import home2 from "../../assets/2.png";
 import home3 from "../../assets/3.jpg";
 import home4 from "../../assets/4.jpg";
 
 const GalleryCarousel = () => {
+  const [countSlider, setCountSlider] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      countSlider === 3 ? setCountSlider(0) : setCountSlider(countSlider + 1);
+    }, 3000);
+  }, [countSlider]);
+
   return (
     <div className="carousel">
-      <div className="carousel-item">
+      <div
+        className="carousel-item"
+        style={{ transform: `translateX(${countSlider * -1200}px)` }}
+      >
         <a href="">
           <img src={home1} alt="" />
         </a>
       </div>
-      <div className="carousel-item">
+      <div
+        className="carousel-item"
+        style={{ transform: `translateX(${countSlider * -1200}px)` }}
+      >
         <a href="">
           <img src={home2} alt="" />
         </a>
       </div>
-      <div className="carousel-item">
+      <div
+        className="carousel-item"
+        style={{ transform: `translateX(${countSlider * -1200}px)` }}
+      >
         <a href="">
           <img src={home3} alt="" />
         </a>
       </div>
-      <div className="carousel-item">
+      <div
+        className="carousel-item"
+        style={{ transform: `translateX(${countSlider * -1200}px)` }}
+      >
         <a href="">
           <img src={home4} alt="" />
         </a>
