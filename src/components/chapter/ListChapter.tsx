bbd01ab046128638f9811920dc32a00bca2c2494
unknown <levietphu171993@gmail.com>
2022-11-18 17:00:34 +0700

    fixed hidden page in popup

--- src/components/chapter/ListChapter.tsx
+++ src/components/chapter/ListChapter.tsx
@@ -75,122 +75,124 @@ const ListChapter = ({
             left: `${togglePopup ? "0" : "-500"}px`,
           }}
         >
-          <div className="header__list">
-            <div className="list">
-              <span>Danh sách chương</span>
-            </div>
-            <div className="sort__search">
-              {orderby === "asc" ? (
-                <button
-                  onClick={() => {
-                    setOrderby("desc");
-                    !checkKeywordOrderby && setCheckKeywordOrderby(true);
-                  }}
-                >
-                  <i className="fa-solid fa-arrow-up-9-1"></i>
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    setOrderby("asc");
-                    !checkKeywordOrderby && setCheckKeywordOrderby(true);
-                  }}
-                >
-                  <i className="fa-solid fa-arrow-down-1-9"></i>
-                </button>
-              )}
-              <div className="search">
-                <div
-                  className={`search__container ${
-                    checkSearchChapter ? "search__chapter--active" : ""
-                  }`}
-                >
-                  <div className="search__icon">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                  </div>
-                  <input
-                    className="search-input"
-                    type="text"
-                    placeholder="Tìm theo số chương, tên chương..."
-                    onClick={() => setCheckSearchChapter(!checkSearchChapter)}
-                    onBlur={() => setCheckSearchChapter(false)}
-                    value={keyword}
-                    onChange={(e) => {
-                      setKeyword(e.target.value);
+          <div style={{ position: "relative" }}>
+            <div className="header__list">
+              <div className="list">
+                <span>Danh sách chương</span>
+              </div>
+              <div className="sort__search">
+                {orderby === "asc" ? (
+                  <button
+                    onClick={() => {
+                      setOrderby("desc");
                       !checkKeywordOrderby && setCheckKeywordOrderby(true);
                     }}
-                  />
+                  >
+                    <i className="fa-solid fa-arrow-up-9-1"></i>
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setOrderby("asc");
+                      !checkKeywordOrderby && setCheckKeywordOrderby(true);
+                    }}
+                  >
+                    <i className="fa-solid fa-arrow-down-1-9"></i>
+                  </button>
+                )}
+                <div className="search">
+                  <div
+                    className={`search__container ${
+                      checkSearchChapter ? "search__chapter--active" : ""
+                    }`}
+                  >
+                    <div className="search__icon">
+                      <i className="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <input
+                      className="search-input"
+                      type="text"
+                      placeholder="Tìm theo số chương, tên chương..."
+                      onClick={() => setCheckSearchChapter(!checkSearchChapter)}
+                      onBlur={() => setCheckSearchChapter(false)}
+                      value={keyword}
+                      onChange={(e) => {
+                        setKeyword(e.target.value);
+                        !checkKeywordOrderby && setCheckKeywordOrderby(true);
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="main__list">
-            {dataChapter.allChapter.data.map((item: any) => {
-              return (
-                <Link
-                  to={`/${params.slugstory}/${item.slug}`}
-                  className={`main__item ${
-                    params.slugchapter === item.slug ? "active" : ""
-                  }`}
-                  key={item.id}
-                >
-                  <div className="item__name__number">
-                    <span className="number">{item.chapter_number}.</span>
-                    <span>
-                      {" "}
-                      {item.name_chapter.length > 17
-                        ? item.name_chapter.slice(0, 14) + "..."
-                        : item.name_chapter}
-                    </span>
-                  </div>
-                  <i>
-                    <span>Cập nhật: </span>
-                    <span>
-                      <Moment fromNow locale="vi">
-                        {item.created_at}
-                      </Moment>
-                    </span>
-                  </i>
-                  {item.bought && (
-                    <div className="bought">
-                      <i className="fa-solid fa-lock-open"></i>
+            <div className="main__list">
+              {dataChapter.allChapter.data.map((item: any) => {
+                return (
+                  <Link
+                    to={`/${params.slugstory}/${item.slug}`}
+                    className={`main__item ${
+                      params.slugchapter === item.slug ? "active" : ""
+                    }`}
+                    key={item.id}
+                  >
+                    <div className="item__name__number">
+                      <span className="number">{item.chapter_number}.</span>
+                      <span>
+                        {" "}
+                        {item.name_chapter.length > 17
+                          ? item.name_chapter.slice(0, 14) + "..."
+                          : item.name_chapter}
+                      </span>
                     </div>
-                  )}
-                  {!item.bought && item.coin > 0 && (
-                    <span className="money">{item.coin} xu </span>
-                  )}
-                </Link>
-              );
-            })}
-          </div>
-          <div className="footer__list">
-            <div className="next__prev">
-              <button
-                className={`footer__list--prev ${
-                  dataChapter.allChapter.current_page === 1 ? "forbidden" : ""
-                }`}
-                onClick={() =>
-                  dataChapter.allChapter.current_page > 1 &&
-                  changePageChapter("prev")
-                }
-              >
-                Trang trước
-              </button>
-              <button
-                className={`footer__list--next ${
-                  dataChapter.allChapter.current_page ===
-                  dataChapter.allChapter.last_page
-                    ? "forbidden"
-                    : ""
-                }`}
-                onClick={() =>
-                  dataChapter.allChapter.current_page !==
-                    dataChapter.allChapter.last_page &&
-                  changePageChapter("next")
-                }
-              >
-                Trang sau
-              </button>
+                    <i>
+                      <span>Cập nhật: </span>
+                      <span>
+                        <Moment fromNow locale="vi">
+                          {item.created_at}
+                        </Moment>
+                      </span>
+                    </i>
+                    {item.bought && (
+                      <div className="bought">
+                        <i className="fa-solid fa-lock-open"></i>
+                      </div>
+                    )}
+                    {!item.bought && item.coin > 0 && (
+                      <span className="money">{item.coin} xu </span>
+                    )}
+                  </Link>
+                );
+              })}
+            </div>
+            <div className="footer__list">
+              <div className="next__prev">
+                <button
+                  className={`footer__list--prev ${
+                    dataChapter.allChapter.current_page === 1 ? "forbidden" : ""
+                  }`}
+                  onClick={() =>
+                    dataChapter.allChapter.current_page > 1 &&
+                    changePageChapter("prev")
+                  }
+                >
+                  Trang trước
+                </button>
+                <button
+                  className={`footer__list--next ${
+                    dataChapter.allChapter.current_page ===
+                    dataChapter.allChapter.last_page
+                      ? "forbidden"
+                      : ""
+                  }`}
+                  onClick={() =>
+                    dataChapter.allChapter.current_page !==
+                      dataChapter.allChapter.last_page &&
+                    changePageChapter("next")
+                  }
+                >
+                  Trang sau
+                </button>
+              </div>
             </div>
           </div>
         </div>
